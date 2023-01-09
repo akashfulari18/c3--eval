964d2a491ccffb8260b218ae938cf8e82d7dae1a
akash <akashfulari007@gmail.com>
2023-01-09 13:55:23 +0530

    c3-eval 0.4

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,7 +36,7 @@ export default function Home({profileData,projectData}) {
           </div>
 
             <div style={{boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset",marginTop:"1rem" ,padding:"1rem"}}>
-              education:
+              Education:
 
               <Experience/>
 
